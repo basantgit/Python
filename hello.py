85040f8514e5fa0ea2b87a494a4f3f8d36d109c6
Basant Kumar Mohanty <basantmohanty7@gmail.com>
2024-03-29 10:40:27 +0000

    update

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-a = ['abc', 'data', 'sam']
-print("a")
+server_name = "my_server"
+port = 80
+is_https_enabled = True
+max_connections = 1000
+
+print(f"Server Name: {Web}")
+print(f"Port: {80}")
+print(f"HTTPS Enabled: {True}")
+print(f"Max Connections: {1000}")
