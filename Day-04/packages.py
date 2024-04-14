8a092426e6ece4f386ddc8edec1b14bf3766edee
Basant Kumar Mohanty <basantmohanty7@gmail.com>
2024-04-14 00:32:32 +0000

    Added workspace venv

---
+++ Day-04/packages.py
@@ -0,0 +1,4 @@
+pip install virtualenv --- #Create virtualenv
+python -m venv project-abc      # Create workspace of projecet-abc name
+source project-abc/bin/ativate  #Sclect projet and go inside it
+deactivate # Exit form workspace project
