92339fb83366cf5d2fa1cd1b87adc7d8e3440c92
Basant Kumar Mohanty <basantmohanty7@gmail.com>
2024-04-21 13:46:22 +0000

    updated

---
+++ data_type.py
@@ -0,0 +1,38 @@
+# # # name = "basant" # variable use for
+# # # age = 35
+# # # point = 2.9
+
+# # # print (type(name))
+# # # print (type(age))
+# # # print (type(point))
+
+# # # ''' variable use for 
+# # # hello this is comments
+# # # '''
+
+# # # name = "Basant Mohanty"
+# # # print(name[0:12])
+
+# # name = "I am studying am python"
+# # print(name.count("am"))
+# # txt = 'i 36 is my age.'
+
+# # x = txt.capitalize()
+
+# # print (x)
+# name = input("Please enter your name: ")
+# print("Lenth is your name:", len(name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
